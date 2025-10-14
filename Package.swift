05fd036028991b4bbbb5a76d2c0a29d9faf93523
Alon Genosar <dubon.yaar.the.3rd@gmail.com>
2025-10-14 15:34:35 +0300

    Fix: Lower Swift tools version to 5.9 for broader compatibility

--- Package.swift
+++ Package.swift
@@ -1,11 +1,11 @@
-// swift-tools-version: 6.2
+// swift-tools-version: 5.9
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "SnazzyRouter",
-    platforms: [.iOS("17.0")],
+    platforms: [.iOS(.v17)],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
